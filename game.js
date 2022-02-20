6e7407fd0a1113bde9ceaab95fcd873ed150ad1d
Alfred2222 <84137430+Alfred2222@users.noreply.github.com>
2022-02-20 14:42:23 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -11,19 +11,22 @@ document.getElementById("player1_score").innerHTML=player1_score
 document.getElementById("player2_name").innerHTML=name_player2+":"
 document.getElementById("player2_score").innerHTML=player2_score
 
-number1_input=document.getElementById("num1").value;
-localStorage.setItem("num1",num1)
+document.getElementById("qturn").innerHTML="Question turn -"+name_player1
+document.getElementById("aturn").innerHTML="Answer turn -"+name_player2
 
-number2_input=document.getElementById("num2").value;
-localStorage.setItem("num2",num2)
+number1_input=document.getElementById("number_1").value
+console.log(number1_input)
 
-number1=localStorage.getItem(num1)
-number2=localStorage.getItem(num2)
-actual_answer=parseInt(number1)*parseInt(number2)
+number2_input=document.getElementById("number_2").value
+console.log(number2_input)
+
+
+actual_answer=parseInt(number1_input)*parseInt(number2_input)
 
 
 function submit(){
-qnum= number1+" x "+number2
+qnum= number1_input+" x "+number2_input
+console.log(qnum)
 question="<h4 id-'num_display'>Q:- "+qnum+"</h4>"
 input_box="</br> Answer: <input type='text' id='input_check_box'>";
 check_button="</br></br><button class='btn btn-info' onclick='check()'> CHECK </button>";
@@ -31,5 +34,39 @@ row=question + input_box + check_button;
 document.getElementById("output").innerHTML=row;
 document.getElementById("num1").value="";
 document.getElementById("num2").value="";
+}
+
+
+question_turn="player1"
+answer_turn="player2"
+
+function check(){
+    if(actual_answer==input_box){
+        if(answer_turn="player2")
+        player2_score=player2_score+1
+        document.getElementById("player2_score").innerHTML=player2_score;
+    }
+    else{
+        player1_score=player1_score+1;
+        document.getElementById("player2_score").innerHTML=player1_score
+    }
+    if(question_turn=="player1"){
+        question_turn="player2"
+        document.getElementById("qturn").innerHTML="Question turn-"+name_player2
+    }
+    else{
+        question_turn="player1"
+        document.getElementById("qturn").innerHTML="Question turn-"+name_player1
+        
+    }
+
+    if(answer_turn=="player1"){
+        answer_turn="player2"
+        document.getElementById("aturn").innerHTML="Answer turn-"+name_player2
+    }
+    else{
+        answer_turn="player2"
+        document.getElementById("aturn").innerHTML="Answer turn-"+name_player1
+    }
 }
 
